0f9982ff89ef275c231124e73bce1c773d386998
Satyam Dalai <dalai.satyam@gmail.com>
2020-10-06 19:56:42 +0530

    update

--- HackerRank/src/com/HackerRank/Algorithms/AlternatingCharacters.java
+++ HackerRank/src/com/HackerRank/Algorithms/AlternatingCharacters.java
@@ -7,30 +7,27 @@ package com.HackerRank.Algorithms;
 import java.util.*;
 
 public class AlternatingCharacters {
-private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
 
-	public static void main(String[] args)
-	{
-		int q =scanner.nextInt();
-
-        for (int qItr = 0; qItr < q; qItr++) {
+        for (int n = 0; n < t; n++) {
             String s = scanner.next();
 
             int result = alternatingCharacters(s);
-            
+
             System.out.println(result);
         }
         scanner.close();
 
-}
-	static int alternatingCharacters(String s) 
-    {
+    }
+
+    static int alternatingCharacters(String s) {
         int count = 0;
-        for(int i=0; i<s.length()-1; i++)
-        {
-            if(s.charAt(i) == s.charAt(i+1))
-                count ++ ;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1))
+                count++;
         }
         return count;
     }
